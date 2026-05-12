5d12edf3a7a7bd50a7c02199cb58fa19e5f1380e
dariapusovskaya <pusovskajadn@gmail.com>
2026-05-12 16:32:38 +0300

    init: homepage code

--- rs-react-app/src/pages/HomePage.tsx
+++ rs-react-app/src/pages/HomePage.tsx
@@ -1,4 +1,93 @@
+import { useState, useEffect } from 'react';
+import { Search } from '../components/Search/Search';
+import Results from '../components/Results/Results';
+import Bomb from '../components/Bomb/Bomb';
+import { fetchItems } from '../services/api';
+import type { Item } from '../types';
+
+
 const HomePage = () => {
-  return <h1>Home Page - Search will be here</h1>;
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [explode, setExplode] = useState(false);
+
+  const performSearch = async (term: string) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const items = await fetchItems(term);
+      setResults(items);
+      setLoading(false);
+    } catch (err) {
+        setResults([]);
+        setLoading(false);
+        setError('cant upload the data')
+    }
+  };
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+    localStorage.setItem('searchTerm', term);
+    performSearch(term);
+  };
+
+  const triggerError = () => {
+    setExplode(true);
+  };
+
+  useEffect(() => {
+    const savedTerm = localStorage.getItem('searchTerm');
+    if (savedTerm) {
+      setSearchTerm(savedTerm);
+      performSearch(savedTerm);
+    } else {
+      performSearch('');
+    }
+  }, []);
+
+  return (
+    <>
+      <div style={{ maxWidth: '800px', margin: '0 auto' }}>
+          <h1 style={{ textAlign: 'center' }}>Search App</h1>
+
+          <Search 
+          searchTerm={searchTerm}
+          onSearch={handleSearch}
+          isLoading={loading}
+          />
+
+          <Results 
+          results={results}
+          loading={loading}
+          error={error}
+          />
+
+           <Bomb shouldExplode={explode} />
+
+           <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+            <button
+              onClick={triggerError}
+              style={{
+              padding: '8px 16px',
+              backgroundColor: '#dc3545',
+              color: 'white',
+              border: 'none',
+              borderRadius: '4px',
+              cursor: 'pointer'
+              }}
+            >
+              Simulate Error
+            </button>
+            </div>
+        </div>
+    </>
+  )
+
+
 };
+
 export default HomePage;
