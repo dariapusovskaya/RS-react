0a84b060c0beaaf8bf4220bc5cb70df54501a5d2
dariapusovskaya <pusovskajadn@gmail.com>
2026-05-12 16:33:00 +0300

    init: about/notfound pages

--- rs-react-app/src/pages/AboutPage.tsx
+++ rs-react-app/src/pages/AboutPage.tsx
@@ -1,4 +1,19 @@
 const AboutPage = () => {
-  return <h1>About Page</h1>;
-};
+  return (
+    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
+      <h1>About This App</h1>
+      <p>Author: dariapusovskaya</p>
+      <p>
+        <a>
+        Course:
+          RS School React Course
+        </a>
+      </p>
+      <p>Version: 1.0.0</p>
+      <p>This app allows you to search products using DummyJSON API.</p>
+    </div>
+  )
+}
+
+
 export default AboutPage;
