51fcf4953725bde55c6d17277d5e6c41d06a39dc
dariapusovskaya <pusovskajadn@gmail.com>
2026-05-18 13:31:13 +0300

    add: react course link

--- rs-react-app/src/pages/AboutPage.tsx
+++ rs-react-app/src/pages/AboutPage.tsx
@@ -4,12 +4,15 @@ const AboutPage = () => {
       <h1>About This App</h1>
       <p>Author: dariapusovskaya</p>
       <p>
-        <a>
-        Course:
+        Course:{' '}
+        <a 
+          href="https://rs.school/courses/reactjs" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           RS School React Course
         </a>
       </p>
-      <p>Version: 1.0.0</p>
       <p>This app allows you to search products using DummyJSON API.</p>
     </div>
   )
